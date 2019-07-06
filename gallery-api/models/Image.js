1157a405fce0e10de63c8f08dc0ba90432dfcf3d
Владислав Цой <tsoy.1996.vlad@gmail.com>
2019-07-06 11:53:46 +0600

    #2 Добавил модели для User и Image

---
+++ gallery-api/models/Image.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+const Image = mongoose.model('Image', ImageSchema);
+
+module.exports = Product;
